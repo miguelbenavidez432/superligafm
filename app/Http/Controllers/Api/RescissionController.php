22f7b17cb0364818ec8eda1a3aabf38cc23a3fbd
miguelbenavidez432 <119986047+miguelbenavidez432@users.noreply.github.com>
2023-11-04 10:13:46 -0300

    fix some controllers

--- app/Http/Controllers/Api/RescissionController.php
+++ app/Http/Controllers/Api/RescissionController.php
@@ -19,7 +19,7 @@ class RescissionController extends Controller
     public function index()
     {
         return RescissionResource::collection(
-            Rescission::query()->orderBy('created_at', 'desc')->paginate(500)
+            Rescission::query()->orderBy('created_at', 'asc')->paginate(500)
         );
     }
 
