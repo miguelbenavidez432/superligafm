f4ef632a7557e8e1c37e2c2ce416d14c3cf0b853
miguelbenavidez432 <119986047+miguelbenavidez432@users.noreply.github.com>
2024-11-27 00:03:13 -0300

    fix auc

--- app/Http/Controllers/Api/AuctionController.php
+++ app/Http/Controllers/Api/AuctionController.php
@@ -151,7 +151,7 @@ class AuctionController extends Controller
         $lastAuctions = Auction::select('id_player', DB::raw('MAX(id) as lastAuctionID'))
         ->groupBy('id_player')
         ->with(['creator', 'auctioneer', 'player', 'team', 'season'])
-        ->where('active' , 'yes')
+        ->where('id' , '>', 56)
         ->orderBy('created_at', 'desc')
         ->get()
         ->map(function (Auction $auction) {
