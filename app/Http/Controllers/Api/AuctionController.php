8599e34cc7ccf596c5202c0514175e867756de13
miguelbenavidez432 <119986047+miguelbenavidez432@users.noreply.github.com>
2025-08-23 12:23:32 -0300

    Se agrega vinculación de usuario de discord y menciones en el servidor

--- app/Http/Controllers/Api/AuctionController.php
+++ app/Http/Controllers/Api/AuctionController.php
@@ -79,11 +79,10 @@ class AuctionController extends Controller
             }
 
             $previousBidders = Auction::where('id_player', $data['id_player'])->get();
-            var_dump($previousBidders);
             foreach ($previousBidders as $bidder) {
                 $user = $bidder->user;
-                $userDiscord = DiscordUser::where('user_id', $user->id)->first();
-                if ($userDiscord && !in_array($userDiscord->discord_id, $idDiscord)) $idDiscord[] = $userDiscord->discord_id;
+                // $userDiscord = DiscordUser::where('user_id', $user->id)->first();
+                // if ($userDiscord && !in_array($userDiscord->discord_id, $idDiscord)) $idDiscord[] = $userDiscord->discord_id;
             }
         } else {
             if ($data['amount'] < $player->value) { // agregar /2 para que sea la mitad del valor del jugador en las subastas extras
